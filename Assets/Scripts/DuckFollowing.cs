b6fced89f0c0210a33855f5c5917e876091e10e2
Rok Kos <rok.kos@outfit7.com>
2021-01-29 23:53:05 +0100

    Better navmesh

---
+++ Assets/Scripts/DuckFollowing.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DuckFollowing : MonoBehaviour
+{
+    NavMeshAgent m_Agent;
+
+    private Transform playerTransform = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTransform != null)
+        {
+            m_Agent.destination = playerTransform.position;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            playerTransform = other.transform;
+            m_Agent.autoBraking = true;
+            m_Agent.isStopped = false;
+        }
+    }
+}
