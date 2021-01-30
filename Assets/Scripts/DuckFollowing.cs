b5485220711f52373677a7e7213866171372dd5b
Rok Kos <rok.kos@outfit7.com>
2021-01-30 19:01:25 +0100

    Spikes implementation

--- Assets/Scripts/DuckFollowing.cs
+++ Assets/Scripts/DuckFollowing.cs
@@ -6,6 +6,8 @@ using UnityEngine.AI;
 
 public class DuckFollowing : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem feather_particles = null;
+    
     NavMeshAgent m_Agent;
 
     private Transform playerTransform = null;
@@ -33,6 +35,14 @@ public class DuckFollowing : MonoBehaviour
             var mamaDuck = mamaObj.GetComponentInParent<MamaDuck>();
             playerTransform = mamaDuck.GetLastDuckInLine();
             m_Agent.autoBraking = true;
+        }
+
+        if (other.tag.Equals("Spikes"))
+        {
+            var feather_obj = Instantiate(feather_particles, transform.position, Quaternion.identity);
+            feather_obj.Play();
+            Destroy(feather_obj.gameObject, 1.0f);
+            Destroy(gameObject);
         }
     }
 }
