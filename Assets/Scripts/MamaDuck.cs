7a68105418e293ac65ab1e90b28addc52825c831
Rok Kos <rok.kos@outfit7.com>
2021-01-30 13:24:20 +0100

    Animating Mamam Duck

--- Assets/Scripts/MamaDuck.cs
+++ Assets/Scripts/MamaDuck.cs
@@ -4,17 +4,39 @@ using System.Collections.Generic;
 using System.Linq;
 using Cinemachine;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MamaDuck : MonoBehaviour
 {
     [SerializeField] private CinemachineTargetGroup TargetGroup;
-
+    [SerializeField] private float scaleFactor = 20f;
+    [SerializeField] private float minVal = 0.8f;
+    [SerializeField] private float sinEffect = 0.8f;
     private List<GameObject> ducksFolowing;
+
+    private Vector3 prevPos;
+    private float scaleValue = 0.0f;
+    private NavMeshAgent navMeshAgent = null;
+    
     
     // Start is called before the first frame update
     void Start()
     {
         ducksFolowing = new List<GameObject>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        prevPos = transform.position;
+    }
+
+    private void Update()
+    {
+        var currentPos = transform.position;
+        
+        scaleValue += ((currentPos - prevPos).magnitude + navMeshAgent.speed) * Time.deltaTime;
+        var scale = transform.localScale;
+        scale.y = minVal + sinEffect * Math.Abs(Mathf.Sin(scaleValue * scaleFactor * Mathf.Deg2Rad));
+        transform.localScale = scale;
+
+        prevPos = currentPos;
     }
 
     private void OnTriggerEnter(Collider other)
