5119e210ae65d7d42a9d4d46a35a53a9d7fed44f
Rok Kos <rok.kos@outfit7.com>
2021-01-31 13:44:03 +0100

    Quit to main menu

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -15,6 +16,22 @@ public class MainMenuScript : MonoBehaviour
 
     public void GoToMain()
     {
-        SceneManager.LoadScene("MainMenu");
+        if (SceneManager.GetActiveScene().name.Equals("MainMenu"))
+        {
+
+            Application.Quit();
+        }
+        else
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GoToMain();
+        }
     }
 }
