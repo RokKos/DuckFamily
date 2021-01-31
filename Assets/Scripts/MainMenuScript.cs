a1bd06397d1f191727ae918fe22050aa530d5cee
Rok Kos <rok.kos@outfit7.com>
2021-01-31 13:32:48 +0100

    Main menu and music low

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+
+    public void Play()
+    {
+        SceneManager.LoadScene("LevelMain");
+    }
+
+    public void GoToMain()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
